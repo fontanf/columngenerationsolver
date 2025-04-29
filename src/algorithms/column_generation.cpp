75cab266391f7b5fb1a6a9afbdbb49a42f61ebf6
Florian Fontan <dev@florian-fontan.fr>
2025-04-29 18:40:09 +0200

    Fix column generation

--- src/algorithms/column_generation.cpp
+++ src/algorithms/column_generation.cpp
@@ -111,6 +111,7 @@ const ColumnGenerationOutput columngenerationsolver::column_generation(
             // currently fixed column or it contains an item / job also
             // included in a currently fixed column.
             if (model.rows[element.row].coefficient_lower_bound >= 0
+                    && column->type == VariableType::Integer
                     && row_values[element.row] + element.coefficient
                     > model.rows[element.row].upper_bound) {
                 ok = false;
@@ -326,7 +327,8 @@ const ColumnGenerationOutput columngenerationsolver::column_generation(
                 // For example, it corresponds to the same bin / machine that a
                 // currently fixed column or it contains an item / job also
                 // included in a currently fixed column.
-                if (element.coefficient >= 0
+                if (model.rows[element.row].coefficient_lower_bound >= 0
+                        && column->type == VariableType::Integer
                         && row_values[element.row] + element.coefficient
                         > model.rows[element.row].upper_bound) {
                     ok = false;
